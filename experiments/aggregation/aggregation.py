a4deaa0bba3410ca0e6d52c94f3c4a18b380e77f
RenskeDiep <57197859+RenskeDiep@users.noreply.github.com>
2021-06-08 17:00:14 +0200

    * containing object created

--- experiments/aggregation/aggregation.py
+++ experiments/aggregation/aggregation.py
@@ -13,18 +13,34 @@ class Aggregations(Swarm):
         if config["aggregation"]["obstacles"]:
             object1_loc = config["aggregation"]["object1_location"]
             object2_loc = config["aggregation"]["object2_location"]
+            con_object_loc = config["aggregation"]["containing_object_location"]
 
             scale1 = config["aggregation"]["object1_scale"]
             scale2 = config["aggregation"]["object2_scale"]
+            con_scale = config["aggregation"]["containing_object_scale"]
 
-            filename = ("experiments/aggregation/images/greyc1.png")
-            self.objects.add_object(file=filename, pos=object1_loc, scale=scale1, obj_type="obstacle")
+            # containing object
+            filename = ("experiments/flocking/images/redd.png")
+            self.objects.add_object(file=filename, pos=con_object_loc, scale=con_scale, obj_type="obstacle")
 
             filename = ("experiments/aggregation/images/greyc2.png")
-            self.objects.add_object(file=filename, pos=object2_loc, scale=scale2, obj_type="obstacle")
+            #site 1
+            self.objects.add_object(file=filename, pos=object1_loc, scale=scale1, obj_type="site")
+            #site 2
+            self.objects.add_object(file=filename, pos=object2_loc, scale=scale2, obj_type="site")
+
+            min_x, max_x = area(con_object_loc[0], con_scale[0])
+            min_y, max_y = area(con_object_loc[1], con_scale[1])
 
 
         for index, agent in enumerate(range(num_agents)):
             coordinates = generate_coordinates(self.screen)
+            while (
+                    coordinates[0] >= max_x
+                    or coordinates[0] <= min_x
+                    or coordinates[1] >= max_y
+                    or coordinates[1] <= min_y
+            ):
+                coordinates = generate_coordinates(self.screen)
 
             self.add_agent(Cockroach(pos=np.array(coordinates), v=None, aggregation=self, index=index))
