29d4dd0c9c68533ff109ce1b5ed2f51c05cd4b59
RenskeDiep <57197859+RenskeDiep@users.noreply.github.com>
2021-06-08 16:04:02 +0200

    * one aggregation section added in initialize

--- experiments/aggregation/aggregation.py
+++ experiments/aggregation/aggregation.py
@@ -10,4 +10,24 @@ class Aggregations(Swarm):
         super(Aggregations, self).__init__(screen)
 
     def initialize(self, num_agents) -> None:
-        pass
+
+        # add obstacle/-s to the environment if present
+        if config["base"]["site"]:
+            object_loc = config["base"]["object_location"]
+
+
+            scale = [200, 200]
+
+            filename = (
+                "experiments/aggregation/images/greyc1.png"
+                if config["base"]["background_grey"]
+                else "experiments/aggregation/images/greyc2.png"
+            )
+
+            self.objects.add_object(
+                file=filename, pos=object_loc, scale=scale, obj_type="site"
+            )
+
+            min_x, max_x = area(object_loc[0], scale[0])
+            min_y, max_y = area(object_loc[1], scale[1])
+
