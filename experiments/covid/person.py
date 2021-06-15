4ac088b02004e53c5681a62fcdc4c2546b6de00d
RenskeDiep <57197859+RenskeDiep@users.noreply.github.com>
2021-06-15 15:03:57 +0200

    * population class works

--- experiments/covid/person.py
+++ experiments/covid/person.py
@@ -8,4 +8,30 @@ from simulation.utils import *
 
 class Person(Agent):
     """ """
-    pass
+    def __init__(
+            self, pos, v, population, index: int, image: str = "experiments/flocking/images/normal-boid.png"
+    ) -> None:
+        """
+                Args:
+                ----
+                    pos:
+                    v:
+                    flock:
+                    index (int):
+                    image (str): Defaults to "experiments/flocking/images/normal-boid.png"
+                """
+        super(Person, self).__init__(
+            pos,
+            v,
+            image,
+            max_speed=config["agent"]["max_speed"],
+            min_speed=config["agent"]["min_speed"],
+            mass=config["agent"]["mass"],
+            width=config["agent"]["width"],
+            height=config["agent"]["height"],
+            dT=config["agent"]["dt"],
+            index=index
+        )
+    def update_actions(self) -> None:
+        pass
+
--- experiments/covid/population.py
+++ experiments/covid/population.py
@@ -17,22 +17,29 @@ class Population(Swarm):
             num_agents (int):
 
         """
+        if config["population"]["obstacles"]:
+            object1_loc = config["population"]["object1_location"]
+            scale1 = config["population"]["scale1"]
+            filename = ("experiments/aggregation/images/greyc2.png")
+            self.objects.add_object(file=filename, pos=object1_loc, scale=scale1, obj_type="obstacle")
 
         # To Do
         # code snipet (not complete) to avoid initializing agents on obstacles
         # given some coordinates and obstacles in the environment, this repositions the agent
-        coordinates = generate_coordinates(self.screen)
+        for index, agent in enumerate(range(num_agents)):
+            coordinates = generate_coordinates(self.screen)
 
-        if config["population"]["obstacles"]:  # you need to define this variable
-            for obj in self.objects.obstacles:
-                rel_coordinate = relative(
-                    coordinates, (obj.rect[0], obj.rect[1])
-                )
-                try:
-                    while obj.mask.get_at(rel_coordinate):
-                        coordinates = generate_coordinates(self.screen)
-                        rel_coordinate = relative(
-                            coordinates, (obj.rect[0], obj.rect[1])
-                        )
-                except IndexError:
-                    pass
+            if config["population"]["obstacles"]:  # you need to define this variable
+                for obj in self.objects.obstacles:
+                    rel_coordinate = relative(
+                        coordinates, (obj.rect[0], obj.rect[1])
+                    )
+                    try:
+                        while obj.mask.get_at(rel_coordinate):
+                            coordinates = generate_coordinates(self.screen)
+                            rel_coordinate = relative(
+                                coordinates, (obj.rect[0], obj.rect[1])
+                            )
+                    except IndexError:
+                        pass
+            self.add_agent(Person(pos=np.array(coordinates), v=None, population=self, index=index))
