41206c1c00b3dd307fb3cd63ac1b407f49116a71
RenskeDiep <57197859+RenskeDiep@users.noreply.github.com>
2021-06-17 15:07:36 +0200

    * colliding stuff

--- experiments/covid/person.py
+++ experiments/covid/person.py
@@ -21,6 +21,7 @@ class Person(Agent):
                     flock:
                     index (int):
                     image (str): Defaults to "experiments/flocking/images/normal-boid.png"
+                    avoided_obstacles (bool):
                 """
         super(Person, self).__init__(
             pos,
@@ -37,6 +38,9 @@ class Person(Agent):
         )
         self.state = self.initial_state()
         self.population = population
+        self.avoided_obstacles: bool = False
+        self.prev_pos = None
+        self.prev_v = None
         self.timer = 0
 
     def initial_state(self):
@@ -61,7 +65,24 @@ class Person(Agent):
         for obstacle in self.population.objects.obstacles:
             collide = pygame.sprite.collide_mask(self, obstacle)
             if bool(collide):
+                # If boid gets stuck because when avoiding the obstacle ended up inside of the object,
+                # resets the position to the previous one and do a 180 degree turn back
+                if not self.avoided_obstacles:
+                    self.prev_pos = self.pos.copy()
+                    self.prev_v = self.v.copy()
+
+                else:
+                    self.pos = self.prev_pos.copy()
+                    self.v = self.prev_v.copy()
+
+                    self.avoided_obstacles = True
                 self.avoid_obstacle()
+                return
+
+            self.prev_v = None
+            self.prev_pos = None
+
+            self.avoided_obstacles = False
 
         neighbors = self.population.find_neighbors(self, config["agent"]["radius_view"])
         if self.state == 'susceptible':
